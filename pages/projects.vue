5320bf6bc8ebc944eb46bd7721f7d75c0bdcf856
mrbeeenopro <127410361+mrbeeenopro@users.noreply.github.com>
2025-04-16 23:00:15 +0700

    Add files via upload

---
+++ pages/projects.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="grid grid-cols-2 gap-4">
+    <ProjectCard
+      title="Docker-optimaze"
+      description="Simple, fast docker java for pteroactyl"
+      cover="/assets/images/projects/pee.jpg"
+      href="https://github.com/mrbeeenopro/dockeroptimaze"
+    />
+    <ProjectCard
+      title="lemem forum"
+      description="a forum by lemem team"
+      cover="/assets/images/projects/blueprint.jpg"
+      href="https://forum.mangovn.xyz"
+    />
+    <ProjectCard
+      title="mchbee.cloud"
+      description="private hosting in Vietnam "
+      cover="/assets/images/projects/logo.jpg"
+      href="https://mchbee.cloud"
+    />
+    <ProjectCard
+      title="idk :3"
+      description="this page powerd by emma"
+      cover="/assets/images/projects/emma.jpg"
+      href="https://prpl.wtf/"
+    />
+  </div>
+</template>
+
+<script>
+import ProjectCard from "~/components/ProjectCard.vue";
+
+export default {
+  components: {
+    ProjectCard,
+  },
+};
+</script>
